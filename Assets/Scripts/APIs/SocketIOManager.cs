72d4f61e1e4958c0bbafa3a055b2ae45896d1d5a
PalaashDwivediUnderpin <palaash.dwivedi@underpinservices.org>
2026-07-31 16:54:36 +0530

    Standard fix

--- Assets/Scripts/APIs/SocketIOManager.cs
+++ Assets/Scripts/APIs/SocketIOManager.cs
@@ -108,6 +108,7 @@ public class SocketIOManager : MonoBehaviour
         options.ConnectWith = Best.SocketIO.Transports.TransportTypes.WebSocket;
 
 #if UNITY_WEBGL && !UNITY_EDITOR
+            JSManager.RegisterAuthTokenListener(gameObject.name); // listen for host's TokenReceived before asking
             JSManager.SendCustomMessage("authToken");
             StartCoroutine(WaitForAuthToken(options));
 #else
